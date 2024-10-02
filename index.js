f3a616e9ecb50595638e4008408a2114df129407
owolabijunior12 <owolabijunior12@gmail.com>
2024-10-02 04:34:11 -0700

    --f

--- index.js
+++ index.js
@@ -47,23 +47,7 @@ function toggleMenu() {
     closeIcon.classList.toggle('hidden');
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    let currentComment = 0;
-    const comments = document.querySelectorAll(".comment-container");
-    const totalComments = comments.length;
 
-    document.getElementById("next-btn").addEventListener("click", function() {
-        comments[currentComment].classList.remove("active");
-        currentComment = (currentComment + 1) % totalComments;
-        comments[currentComment].classList.add("active");
-    });
-
-    document.getElementById("prev-btn").addEventListener("click", function() {
-        comments[currentComment].classList.remove("active");
-        currentComment = (currentComment - 1 + totalComments) % totalComments;
-        comments[currentComment].classList.add("active");
-    });
-});
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
