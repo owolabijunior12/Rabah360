ff866727e6d1beb1da54f9822d80bf26be8d2f25
owolabijunior12 <owolabijunior12@gmail.com>
2024-10-06 08:32:10 -0700

    done landing page desktop

--- index.js
+++ index.js
@@ -1,137 +1,135 @@
-window.onload = function() {
-    setTimeout(function() {
-      document.getElementById('loader').style.display = 'none'; 
-      document.getElementById('main-content').style.display = 'block'; 
-    }, 1000); 
-  };
-  
-  function showLoader() {
-    document.getElementById('loader').style.display = 'flex'; 
-    setTimeout(function() {
-      document.getElementById('loader').style.display = 'none'; 
-    }, 1000);
-  }
+window.onload = () => {
+  setTimeout(() => {
+      toggleVisibility('loader', 'none');
+      toggleVisibility('main-content', 'block');
+  }, 1000);
+};
 
-  function toggleDetails(element) {
-   
-    const detailsContent = element.querySelector('.details-content');
-
-   
-    if (detailsContent.style.display === "none" || detailsContent.style.display === "") {
-        detailsContent.style.display = "block";
-    } else {
-        detailsContent.style.display = "none";
-    }
+// Utility functions
+function toggleVisibility(elementId, displayStyle) {
+  document.getElementById(elementId).style.display = displayStyle;
 }
-document.getElementById('scrollToPricingButton').addEventListener('click', function() {
-  const pricingDiv = document.getElementById('pricing');
-  pricingDiv.scrollIntoView({
-      behavior: 'smooth', // Smooth scrolling
-      block: 'start' // Scroll to the start of the element
+
+function showLoader(timeout = 1000) {
+  toggleVisibility('loader', 'flex');
+  setTimeout(() => {
+      toggleVisibility('loader', 'none');
+  }, timeout);
+}
+
+function toggleDetails(element) {
+  const detailsContent = element.querySelector('.details-content');
+  detailsContent.style.display = detailsContent.style.display === "block" ? "none" : "block";
+}
+
+// Event listeners
+document.getElementById('scrollToPricingButton')?.addEventListener('click', () => {
+  scrollToElement('pricing');
+});
+
+document.getElementById('playButton')?.addEventListener('click', () => {
+  playVideo();
+});
+
+document.getElementById('myButton')?.addEventListener('click', (event) => {
+  event.preventDefault();
+  showLoader();
+});
+
+document.getElementById('myLink')?.addEventListener('click', (event) => {
+  event.preventDefault();
+  showLoader();
+});
+
+document.getElementById('loginForm')?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  handleLoginFormSubmission();
+});
+
+// Scroll to element
+function scrollToElement(elementId) {
+  document.getElementById(elementId)?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
   });
-});
+}
 
-const playButton = document.getElementById('playButton');
-const video = document.getElementById('video');
+// Video controls
+function playVideo() {
+  const video = document.getElementById('video');
+  toggleVisibility('playButton', 'none');
+  video.controls = true;
+  video.play();
+}
 
-
-video.controls = false;
-
-playButton.addEventListener('click', () => {
-    playButton.style.display = 'none'; 
-    // video.controls = true; 
-    video.play(); 
-});
-
-
-  document.getElementById('myButton').addEventListener('click', function(event) {
-    event.preventDefault(); 
-    showLoader(); 
-  });
-
-  document.getElementById('myLink').addEventListener('click', function(event) {
-    event.preventDefault(); 
-    showLoader(); 
-});
+// Mobile menu toggle
 function toggleMenu() {
-    const mobileNavbar = document.getElementById('mobileNavbar');
-    const mobilebtn = document.getElementById('mobile-btn');
-    const menuIcon = document.getElementById('menuIcon');
-    const closeIcon = document.getElementById('closeIcon');
+  const mobileNavbar = document.getElementById('mobileNavbar');
+  const mobileBtn = document.getElementById('mobile-btn');
+  const menuIcon = document.getElementById('menuIcon');
+  const closeIcon = document.getElementById('closeIcon');
 
-    mobileNavbar.classList.toggle('active');
-    mobilebtn.classList.toggle('active');
-    
-    menuIcon.classList.toggle('hidden'); 
-    closeIcon.classList.toggle('hidden');
+  mobileNavbar.classList.toggle('active');
+  mobileBtn.classList.toggle('active');
+  menuIcon.classList.toggle('hidden');
+  closeIcon.classList.toggle('hidden');
 }
 
+// Login form handling
+function handleLoginFormSubmission() {
+  toggleVisibility('loader', 'block');
+  document.getElementById('submit-btn').disabled = true;
 
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+  setTimeout(() => {
+      document.getElementById('loginForm').submit();
+  }, 1000);
+}
 
-
-    document.getElementById('loader').style.display = 'block';
-
-
-    document.getElementById('submit-btn').disabled = true;
-
-
-    setTimeout(function() {
-    
-        document.getElementById('loginForm').submit();
-    }, 1000);
-})
-
-
- 
-  function applyDarkModeToElements() {
-    const elements = document.querySelectorAll(
-      '.second-container, .third-container, .quickly-div, .start-div, .flex-div, .person, .second-grouped-div, .sixth-grouped-div, .comment-container, .msg-div, .quickly-look-div, .grps-div, .event-div, .giving-div, .help-container div, .help-container div a, .first-container, .our-story, .service-div.form, .fifth-groupd-div, .cs-div, .faq, .first-div-contact, .first-service-container,.longdiv,.forth-grouped-div ,.form,.form input,.form textarea,.first-container-abt'
-    );
-
-    elements.forEach((el) => {
-      el.classList.add('dark-mode');
-    });
+// Theme management
+document.addEventListener('DOMContentLoaded', () => {
+  const theme = localStorage.getItem('theme') || 'light';
+  if (theme === 'dark') {
+      applyDarkMode();
+  } else {
+      applyLightMode();
   }
+});
 
- 
-  function removeDarkModeFromElements() {
-    const elements = document.querySelectorAll(
-      '.second-container, .third-container, .quickly-div, .start-div, .flex-div, .person, .second-grouped-div, .sixth-grouped-div, .comment-container, .msg-div, .quickly-look-div, .longdiv,.grps-div, .event-div, .giving-div, .help-container div, .help-container div a, .first-container, .our-story, .service-div.form, .fifth-groupd-div, .cs-div, .faq, .first-div-contact, .first-service-container,.forth-grouped-div ,.form,.form input,.form textarea,.first-container-abt'
-    );
-
-    elements.forEach((el) => {
-      el.classList.remove('dark-mode');
-    });
-  }
-
-  document.addEventListener('DOMContentLoaded', () => {
-   
-    const theme = localStorage.getItem('theme') || 'light';
-
-   
-    if (theme === 'dark') {
-      document.body.classList.add('dark-mode');
-      applyDarkModeToElements();
-    } else {
-      document.body.classList.add('light-mode');
-    }
-  });
-
- 
-  function themes() {
-    const body = document.body;
-    const currentTheme = localStorage.getItem('theme') || 'light';
-
-    if (currentTheme === 'dark') {
-      body.classList.remove('dark-mode');
+function themes() {
+  const currentTheme = localStorage.getItem('theme') || 'light';
+  if (currentTheme === 'dark') {
+      applyLightMode();
       localStorage.setItem('theme', 'light');
-      removeDarkModeFromElements();
-    } else {
-      body.classList.add('dark-mode');
+  } else {
+      applyDarkMode();
       localStorage.setItem('theme', 'dark');
-      applyDarkModeToElements();
-    }
   }
+}
 
+function applyDarkMode() {
+  document.body.classList.add('dark-mode');
+  toggleDarkModeForElements(true);
+}
+
+function applyLightMode() {
+  document.body.classList.remove('dark-mode');
+  toggleDarkModeForElements(false);
+}
+
+function toggleDarkModeForElements(enableDarkMode) {
+  const elements = document.querySelectorAll(
+      '.second-container, .third-container, .quickly-div, .start-div, .flex-div, .person, .second-grouped-div, ' +
+      '.sixth-grouped-div, .comment-container, .msg-div, .quickly-look-div, .grps-div, .event-div, .giving-div, ' +
+      '.help-container div, .help-container div a, .first-container, .our-story, .service-div.form, ' +
+      '.fifth-groupd-div, .cs-div, .faq, .first-div-contact, .first-service-container,.longdiv,.forth-grouped-div, ' +
+      '.form, .form input, .form textarea, .first-container-abt'
+  );
+  
+  elements.forEach((el) => {
+      if (enableDarkMode) {
+          el.classList.add('dark-mode');
+      } else {
+          el.classList.remove('dark-mode');
+      }
+  });
+}
